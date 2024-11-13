ab5d27da0a8a9689fa4bb0b7f9d9293c1ed8b49f
StevenMODIMO <stevepetes26@gmail.com>
2024-11-13 21:06:45 +0300

    quick-cart: version 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,13 @@ interface Products {
 }
 
 export default async function Home() {
-  const response = await fetch("http://localhost:3000/api/admin");
-  const products: Products[] = await response.json();
+  // const response = await fetch("http://localhost:3000/api/admin");
+  // const products: Products[] = await response.json();
 
 
   return (
     <div className="w-[80%] mx-auto">
-      <header className="font-bold text-3xl">
-        <h1>Posts</h1>
-      </header>
-      {products.map(({ _id, productName, price, fileUrl }) => (
+      {/* {products.map(({ _id, productName, price, fileUrl }) => (
         <section key={_id}>
           <div>
             <Image src={fileUrl} alt={productName} width={40} height={40} />
@@ -26,7 +23,7 @@ export default async function Home() {
           <p>{productName}</p>
           <p>{price}</p>
         </section>
-      ))}
+      ))} */}
     </div>
   );
 }
