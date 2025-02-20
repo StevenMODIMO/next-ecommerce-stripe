2cf8dc94fbc0bc036f3b230c88b313b365c36fbd
StevenMODIMO <stevepetes26@gmail.com>
2025-02-20 20:49:38 +0300

    quick-cart: version 1

--- src/components/ProductListings.tsx
+++ src/components/ProductListings.tsx
@@ -1,9 +1,16 @@
-"use client"
-import { useState ,useEffect  } from 'react'
+"use client";
+
+interface ProductListingsTypes {
+  product_id: number;
+  product_name: string;
+  product_description: string;
+  product_category: string;
+  price: number;
+  quantity: number;
+}
+
+import { useState, useEffect } from "react";
 
 export default function ProductListings() {
-    
-    return (
-        <div>ProductListings Section</div>
-    )
+  return <div>ProductListings Section</div>;
 }
