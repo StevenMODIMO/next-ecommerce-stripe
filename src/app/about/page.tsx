35646e8056ccea5fdd13c1f65afbab9ec71568ce
StevenMODIMO <stevepetes26@gmail.com>
2025-02-21 15:12:53 +0300

    quick-cart: versio 1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next"
+
+export default function About() {
+   return (
+    <div>
+      <p className="text-2xl text-center p-4 font-bold">About Us</p>
+   </div>
+  )
+}
