3fe89c92761799c50264b7813afae5b321010878
StevenMODIMO <stevepetes26@gmail.com>
2025-03-03 20:50:43 +0300

    quick-cart: version 1

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
   try {
