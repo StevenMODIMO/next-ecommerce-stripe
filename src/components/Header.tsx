8db929e403e9980a172ebb4d4653e933969170ae
StevenMODIMO <stevepetes26@gmail.com>
2025-03-11 11:52:54 +0300

    quick-cart: version 1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import logo from "@/app/assets/cart-logo.svg";
 import { IoCartOutline } from "react-icons/io5";
 import { MdOutlineAccountCircle, MdOutlineNewLabel } from "react-icons/md";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import Link from "next/link";
 import MobileNav from "@/components/MobileNav";
 import { useState, useEffect } from "react";
@@ -16,11 +16,14 @@ import {
 import { BiSolidLogIn } from "react-icons/bi";
 import { AnimatePresence } from "motion/react";
 import Profile from "./auth/Profile";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
   const [showLinks, setShowLinks] = useState(false);
   const [cartCount, setCartCount] = useState(0);
   const [showProfile, setShowProfile] = useState(false);
+  const { data: session } = useSession();
+  const user = session?.user;
 
   useEffect(() => {
     const updateCartCount = () => {
@@ -29,11 +32,10 @@ export default function Header() {
     };
 
     updateCartCount();
-
     window.addEventListener("cartUpdated", updateCartCount);
-
     return () => window.removeEventListener("cartUpdated", updateCartCount);
   }, []);
+
   return (
     <nav className="fixed left-0 top-0 w-full flex justify-between p-3 z-[50] sm:z-50 text-gray-700 bg-white">
       <header className="relative w-96">
@@ -48,35 +50,39 @@ export default function Header() {
       </header>
       <div className="flex items-center gap-4">
         <div className="hidden lg:flex lg:gap-3 lg:mr-6">
-          {navLinks.map(({ id, name, path }) => (
-            <Link
-              className="text-sm flex items-center gap-2 lg:text-[15px] hover:border-b-2 hover:border-[#E27210] transition-all duration-75 ease-in-out"
-              onClick={() => setShowLinks(false)}
-              key={id}
-              href={path}
-            >
-              {name === "Contact" ? (
-                <TbPhoneCall />
-              ) : name === "Features" ? (
-                <MdOutlineFeaturedPlayList />
-              ) : name === "Testimonials" ? (
-                <MdMotionPhotosPaused />
-              ) : name === "Shop" ? (
-                <TbShoppingCartBolt />
-              ) : name === "SignUp" ? (
-                <MdOutlineNewLabel />
-              ) : name === "SignIn" ? (
-                <BiSolidLogIn />
-              ) : (
-                ""
-              )}
-              {name}
-            </Link>
-          ))}
+          {navLinks
+            .filter(
+              ({ name }) => !(user && (name === "SignUp" || name === "SignIn"))
+            )
+            .map(({ id, name, path }) => (
+              <Link
+                className="text-sm flex items-center gap-2 lg:text-[15px] hover:border-b-2 hover:border-[#E27210] transition-all duration-75 ease-in-out"
+                onClick={() => setShowLinks(false)}
+                key={id}
+                href={path}
+              >
+                {name === "Contact" ? (
+                  <TbPhoneCall />
+                ) : name === "Features" ? (
+                  <MdOutlineFeaturedPlayList />
+                ) : name === "Testimonials" ? (
+                  <MdMotionPhotosPaused />
+                ) : name === "Shop" ? (
+                  <TbShoppingCartBolt />
+                ) : name === "SignUp" ? (
+                  <MdOutlineNewLabel />
+                ) : name === "SignIn" ? (
+                  <BiSolidLogIn />
+                ) : (
+                  ""
+                )}
+                {name}
+              </Link>
+            ))}
         </div>
         <Link href="/cart" className="relative">
           <IoCartOutline className="text-2xl" />
-          <p className="absolute -top-1 -right-2 text-xs bg-[#E27210] rounded-full text-white w-4 h-4 tflex text-center">
+          <p className="absolute -top-1 -right-2 text-xs bg-[#E27210] rounded-full text-white w-4 h-4 text-center">
             {cartCount}
           </p>
         </Link>
