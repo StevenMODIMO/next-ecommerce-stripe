4969623d27d1b0d8b69cf5f21ef295ff9e9a22c0
Steven Modimo <81316521+StevenMODIMO@users.noreply.github.com>
2025-03-20 16:26:27 +0300

    quick-cart: version 1

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,16 +3,16 @@ export default function Loading() {
     <div>
       <div className="max-w-7xl mx-auto px-4 sm:px-12 lg:px-16">
         <div className="flex flex-col gap-2">
-          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto rounded w-full h-5 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto w-full h-24 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto rounded w-full h-5 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto w-full h-24 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
-          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#23b142] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-5 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto w-full h-24 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-5 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto w-full h-24 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
+          <div className="mx-auto rounded w-full h-2 bg-gradient-to-r from-gray-200 to-[#e27210] animate-pulse p-1"></div>
         </div>
       </div>
     </div>
