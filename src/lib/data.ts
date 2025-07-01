8fbf7614efaebee30b40c9ecefb3c7bbd457160b
StevenMODIMO <stevepetes26@gmail.com>
2025-07-01 13:23:42 +0300

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,21 +1,4 @@
-interface Feature {
-  id: number;
-  title: string;
-  description: string;
-}
-
-interface Testimony {
-  id: number;
-  name: string;
-  description: string;
-  title: string;
-}
-
-interface Category {
-  id: number;
-  title: string;
-  value: string
-}
+import {Feature, Testimony, Category } from "@/types/types"
 
 export const categories: Category[] = [
   {
